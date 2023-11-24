ed9d352e09fb010ddd69d493075adafa6ceb1ad0
Durell Hart <rellsadmin@Durells-MacBook-Pro.local>
2023-11-24 00:01:50 -0500

    Second git commit with azure files

---
+++ azure_cli/azure_cli_bash2.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+az vm restart -g MyResorceGroup -n MyVm
+az find blob
+az storage blob --help
---
+++ azure_cloudshell/azure_account_script.ps1
@@ -0,0 +1,3 @@
+Get-AzSubscription
+$context = Get-AzSubscription -SubscriptionId <Get-AzSubscription output>
+Set-AzContext $context 
