4ed701161833fb90ad9b599eda23badc337bd246
Durell Hart <rellsadmin@Durells-MacBook-Pro.local>
2023-11-24 21:19:56 -0500

    format adjustments made

--- azure_cli/azure_cli_bash1.sh
+++ azure_cli/azure_cli_bash1.sh
@@ -2,20 +2,24 @@
 #git remote show origin
 #git remote set-url origin <url>
 #git remote set-url --push origin <url>
+#--------------------------------------------------------------------------#
 LOCATION=$(az group show --name 'NetworkWatcherRG' --query location --out tsv)
 RGNAME='NetworkWatcherRG'
 az group create --name $RGNAME --location $LOCATION
 az group show --name $RGNAME
 az group show --name $RGNAME --location $LOCATION
 DISKNAME='az104-disk1'
+#--------------------------------------------------------------------------#
 az disk create \
 --resource-group $RGNAME \
 --name $DISKNAME \
 --sku 'Standard_LRS' \
 --size-gb 32
+#--------------------------------------------------------------------------#
 az disk show --resource-group $RGNAME --name $DISKNAME
 az disk update --resource-group $RGNAME --name $DISKNAME --size-gb 64
 az disk show --resource-group $RGNAME --name $DISKNAME --query diskSizeGb
 az disk update --resource-group $RGNAME --name $DISKNAME --sku 'Premium_LRS'
 az disk show --resource-group $RGNAME --name $DISKNAME --query sku
 az disk delete --resource-group $RGNAME --disk-name $DISKNAME
+#--------------------------------------------------------------------------#
--- azure_cloudshell/azure_policy_script.ps1
+++ azure_cloudshell/azure_policy_script.ps1
@@ -1,4 +1,6 @@
+#--------------------------------------------------------------------------#
 New-AzResourceGroup -Name 'Staging_VMs' -Location 'Central US' -Force
 $rg = Get-AzResourceGroup -Name "staging_VMs" -Location "Central US"
 $definition = Get-AzPolicyDefinition -Id '/providers/Microsoft.Authorization/policyDefinitions/0a914e76-4921-4c19-b460-a2d36003525a'
 New-AZPolicyAssignment -Scope $rg.resourceId -PolicyDefinition $definition -Name 'RGLocationMatch' -DisplayName 'Resource group matches resources location'
+#--------------------------------------------------------------------------#
